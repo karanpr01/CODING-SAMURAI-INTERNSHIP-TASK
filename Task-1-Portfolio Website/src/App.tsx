443f4e5a8e345311b34b78b670d19288d4cc9a94
Prem <karanprem01@gmail.com>
2025-08-22 13:26:22 +0530

    Added contact me section and link Formspree

--- Task-1-Portfolio Website/src/App.tsx
+++ Task-1-Portfolio Website/src/App.tsx
@@ -1,4 +1,5 @@
 import About from './components/About'
+import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Project'
@@ -10,6 +11,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Projects/>
+      <Contact/>
     </div>
   )
 }
