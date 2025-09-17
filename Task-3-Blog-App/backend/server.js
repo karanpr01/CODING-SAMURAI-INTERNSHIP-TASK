af0b8676c0e5117ce9e4cb78a0196dda2b146c00
Prem <karanprem01@gmail.com>
2025-09-17 13:19:34 +0530

    fixe some error

--- Task-3-Blog-App/backend/server.js
+++ Task-3-Blog-App/backend/server.js
@@ -19,5 +19,7 @@ app.use("/api/blogs", blogRoutes);
 
 app.get("/", (req, res) => res.send("API running"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("server is running on PORT:" + PORT));
+}
