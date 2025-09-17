be10e9eefe65aa9c6436999a695f62eeae83845a
Prem <karanprem01@gmail.com>
2025-09-17 13:22:29 +0530

    inital commit

--- Task-3-Blog-App/backend/server.js
+++ Task-3-Blog-App/backend/server.js
@@ -23,3 +23,6 @@ if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => console.log("server is running on PORT:" + PORT));
 }
+
+// Export server for vercel
+export default server;
